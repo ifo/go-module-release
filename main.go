f5d3e70173bac40c67eb58f74c36f6877afb1a74
Steve McCarthy <steve@redlua.com>
2019-02-25 19:30:09 -0500

    Add Version type and parsing from string to Version

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -12,7 +14,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(version)
+
+	v, err := ParseVersion(version)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(v)
+}
+
+type Version struct {
+	Major int
+	Minor int
+	Patch int
 }
 
 func VersionString() (string, error) {
@@ -21,4 +34,26 @@ func VersionString() (string, error) {
 		return "", err
 	}
 	return strings.Split(string(out), "-")[0], nil
+}
+
+var regex = regexp.MustCompile(`^v(?P<major>\d*)\.(?P<minor>\d*)\.(?P<patch>\d*)`)
+
+func ParseVersion(s string) (Version, error) {
+	captures := regex.FindStringSubmatch(s)
+	if len(captures) != 4 {
+		return Version{}, fmt.Errorf("non-parseable version: %s\n", s)
+	}
+	versions := []int{}
+	for _, c := range captures[1:] {
+		v, err := strconv.Atoi(c)
+		if err != nil {
+			return Version{}, err
+		}
+		versions = append(versions, v)
+	}
+	return Version{
+		Major: versions[0],
+		Minor: versions[1],
+		Patch: versions[2],
+	}, nil
 }
